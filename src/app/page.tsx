b3f3388eb449e8e34cbcf0d6cbc9115969de82b9
kahkashan25SH <ahmedalikahkashan@gmail.com>
2024-12-19 11:05:30 +0500

    added second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Section from "@/components/Section";
 import Cards from "@/components/Cards";
 import OurBlog from "@/components/Our-Blog";
+import React from "react";
 
 export default function Home() {
   return (
